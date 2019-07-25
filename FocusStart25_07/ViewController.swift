ac240d456a96bc00ec7cda60f37862e0b115bfcc
Oleg Krylov <Fastrage@mail.ru>
2019-07-25 19:05:06 +0700

    Initial Commit

---
+++ FocusStart25_07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusStart25_07
+//
+//  Created by Олег Крылов on 25/07/2019.
+//  Copyright © 2019 OlegKrylov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
