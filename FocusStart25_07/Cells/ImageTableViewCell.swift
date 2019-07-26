3f1e6b5322294fab56fa5ccf27ab28c2b3c0045c
Oleg Krylov <Fastrage@mail.ru>
2019-07-26 22:41:36 +0700

    init commit

---
+++ FocusStart25_07/Cells/ImageTableViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  ImageTableViewCell.swift
+//  FocusStart25_07
+//
+//  Created by Олег Крылов on 25/07/2019.
+//  Copyright © 2019 OlegKrylov. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func cellConfigure(url: String) {
+        
+        self.myImage.image = nil
+        self.activityIndicator.startAnimating()
+        self.activityIndicator.hidesWhenStopped = true
+        
+        guard let imageUrl = URL(string: url) else {
+            print("Bad URL")
+            return
+        }
+        
+        let cache = URLCache.shared
+        let request = URLRequest(url: imageUrl)
+        
+        let queue = DispatchQueue.global(qos: .userInteractive)
+        queue.async {
+            if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+                DispatchQueue.main.async { [weak self] in
+                    self?.myImage.image = image
+                    self?.activityIndicator.stopAnimating()
+                }
+            } else {
+                let config = URLSessionConfiguration.default
+                let session = URLSession(configuration: config)
+                let task = session.downloadTask(with: imageUrl)  { imageUrl, response, error in
+                    
+                    guard error == nil else {
+                        print("error: \(error!)")
+                        return
+                    }
+                    guard let content = imageUrl else {
+                        print("no data")
+                        return
+                    }
+                    guard let data = try? Data.init(contentsOf: content) else {
+                        return
+                    }
+                    guard let image = UIImage.init(data: data) else {
+                        
+                        return }
+                    guard let respone = response else {
+                        print("no response")
+                        return
+                    }
+                    let cachedData = CachedURLResponse(response: respone, data: data)
+                    
+                    cache.storeCachedResponse(cachedData, for: request)
+                    DispatchQueue.main.async { [weak self] in
+                        self?.myImage.image = image
+                        //                    self?.myImage.sizeToFit()
+                        self?.activityIndicator.stopAnimating()
+                    }
+                }
+                task.resume()
+            }
+        }
+    }
+}
--- FocusStart25_07/ViewController.swift
+++ FocusStart25_07/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let urls = ImageUrls().urls
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.urls.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCellIdentifier", for: indexPath) as! ImageTableViewCell
+        
+        cell.cellConfigure(url: urls[indexPath.row])
+        return cell
+    }
+    
+    
+}
